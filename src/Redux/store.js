478cbb7969d729ab4dda61d8c2664ea34b857dea
gev707 <gev.mar92@gmail.com>
2021-04-27 23:02:14 +0400

    finished project

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -6,7 +6,6 @@ import singleCardReducer from './reducers/singleCardReducer'
 import contactReducer from './reducers/contactReducer'
 import modalReducer from './reducers/modalReducer';
 import searchReducer from './reducers/searchReducer'
-import logger from 'redux-logger'
 const reducer = combineReducers({
     todoState: todoReducer,
     globalState: globalReducer,
@@ -16,6 +15,5 @@ const reducer = combineReducers({
     searchState: searchReducer
 
 })
-const store = createStore(reducer, applyMiddleware(thunk, logger));
-window.store = store
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store
